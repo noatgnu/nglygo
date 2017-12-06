1392bcfd8579cf6a1263ad232ccd712d665bad9b
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2017-12-06 15:30:01 +1000

    update tree

--- processTree.py
+++ processTree.py
@@ -6,13 +6,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-ct", type=str, dest="ct")
 parser.add_argument("-asrt", type=str, dest="asrt")
 parser.add_argument("-o", type=str, dest="out")
-if "__name__" == "__main__":
-    a = parser.parse_args()
-    currentTree = Phylo.read(a.ct)
-    reconstructed = Phylo.read(a.asrt)
-    for c1, c2 in zip(currentTree.find_clades(order="level"), reconstructed.find_clades(order="level")):
-        if "_" not in c2.name:
-            c1.name = c2.name
-    with open(a.out, "wt", newline="") as outFile:
-        Phylo.write(currentTree, outFile, "newick")
-        outFile.flush()
+
+print("test")
+a = parser.parse_args()
+currentTree = Phylo.read(a.ct, format="newick")
+reconstructed = Phylo.read(a.asrt, format="newick")
+for c1, c2 in zip(currentTree.find_clades(), reconstructed.find_clades()):
+    print(c1.name,c2.name)
+
+with open(a.out, "wt", newline="") as outFile:
+    Phylo.write(currentTree, outFile, "newick")
+    outFile.flush()
